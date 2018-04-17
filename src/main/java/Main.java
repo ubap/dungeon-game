dd3c6b0924dcf2ca836858ab5ac56b4ec3a6f795
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2018-04-17 16:07:56 +0200

    turn north, turn east slower

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,9 +37,9 @@ public class Main {
 
         while (true) {
             protocolGame.sendTurnEast();
-            Thread.sleep(500);
+            Thread.sleep(50000);
             protocolGame.sendTurnNorth();
-            Thread.sleep(500);
+            Thread.sleep(50000);
         }
 
     }
