8755177baab4d6642f028e003b1e40ddd7a68b7c
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2018-04-19 00:43:16 +0200

    refactor field names

--- src/main/java/dunegon/net/InputMessage.java
+++ src/main/java/dunegon/net/InputMessage.java
@@ -4,49 +4,49 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class InputMessage {
-    private ByteBuffer mByteBuffer;
-    private int mMessageSize;
+    private ByteBuffer byteBuffer;
+    private int messageSize;
 
     public InputMessage() {
-        mByteBuffer = ByteBuffer.allocate(65535);
-        mByteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer = ByteBuffer.allocate(65535);
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
 
         reset();
     }
 
     public void setMessageSize(int messageSize) {
-        mMessageSize = messageSize;
+        this.messageSize = messageSize;
     }
 
     public short getU8() {
-        byte[] bytes = mByteBuffer.array();
-        int position = mByteBuffer.position();
+        byte[] bytes = byteBuffer.array();
+        int position = byteBuffer.position();
         short val = (short) (bytes[position] & ((short) 0xFF));
-        mByteBuffer.position(position + 1);
+        byteBuffer.position(position + 1);
         return val;
     }
 
     public int getU16() {
-        byte[] bytes = mByteBuffer.array();
-        int position = mByteBuffer.position();
+        byte[] bytes = byteBuffer.array();
+        int position = byteBuffer.position();
         int val =  bytes[position + 1]<<8 & 0xFF00 | bytes[position] & 0xFF;
-        mByteBuffer.position(position + 2);
+        byteBuffer.position(position + 2);
         return val;
     }
 
     public int peekU16() {
-        byte[] bytes = mByteBuffer.array();
-        int position = mByteBuffer.position();
+        byte[] bytes = byteBuffer.array();
+        int position = byteBuffer.position();
         int val =  bytes[position + 1]<<8 & 0xFF00 | bytes[position] & 0xFF;
         return val;
     }
 
     public long getU32() {
-        byte[] bytes = mByteBuffer.array();
-        int position = mByteBuffer.position();
+        byte[] bytes = byteBuffer.array();
+        int position = byteBuffer.position();
         long val = bytes[position + 3]<<24 & 0xFF000000 | bytes[position + 2]<<16 & 0xFF0000
                 | bytes[position + 1]<<8 & 0xFF00 | bytes[position] & 0xFF;
-        mByteBuffer.position(position + 4);
+        byteBuffer.position(position + 4);
         return val;
     }
 
@@ -58,32 +58,32 @@ public class InputMessage {
     }
 
     public double getDouble() {
-        int precision = mByteBuffer.get();
+        int precision = byteBuffer.get();
         long v = getU32() - Integer.MAX_VALUE;
         return (v / Math.pow(10, precision));
     }
 
     public String getString() {
-        int length = mByteBuffer.getShort();
-        String val = new String(mByteBuffer.array(), mByteBuffer.position(), length);
-        mByteBuffer.position(mByteBuffer.position() + length);
+        int length = byteBuffer.getShort();
+        String val = new String(byteBuffer.array(), byteBuffer.position(), length);
+        byteBuffer.position(byteBuffer.position() + length);
         return val;
     }
 
     public boolean hasMore() {
-        return mByteBuffer.position() < mMessageSize;
+        return byteBuffer.position() < messageSize;
     }
 
     public void reset() {
-        mMessageSize = 0;
-        mByteBuffer.position(0);
+        messageSize = 0;
+        byteBuffer.position(0);
     }
 
     public int getPosition() {
-        return mByteBuffer.position();
+        return byteBuffer.position();
     }
 
     public byte[] getBuffer() {
-        return mByteBuffer.array();
+        return byteBuffer.array();
     }
 }
