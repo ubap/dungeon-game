a7c04908e96949c585f3d7b395799488a70fe332
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2018-04-19 00:35:38 +0200

    remove todo

--- src/main/java/dunegon/net/InputMessage.java
+++ src/main/java/dunegon/net/InputMessage.java
@@ -3,7 +3,6 @@ package dunegon.net;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-// TODO: getU64
 public class InputMessage {
     private ByteBuffer mByteBuffer;
     private int mMessageSize;
